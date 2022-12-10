9820d51469daa7e13937cbbb09d15ec5eb5300c9
AbikeCode <thompsongbemisola9.tg@gmail.com>
2022-12-10 07:46:35 -0800

    print three combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - prints all combination of single digits
- * Return: Always 0
+ * main - Entry point
+ * 
+ * Return: 0
  */
 int main(void)
 {
-	int n;
+	int i;
 
-	for (n = 48; n < 58; n++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(i);
+		if (i != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
