b8ff403f9c1e97fadb44e7440ffb4a0847637331
AbikeCode <thompsongbemisola9.tg@gmail.com>
2022-12-21 14:24:00 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strcnp - compares two strings values
+ * _strcmp - compares two strings values
  * @s1: string 1
  * @s2: string 2
  * Return: the difference in s1 and s2
  */
-int _strcnp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
